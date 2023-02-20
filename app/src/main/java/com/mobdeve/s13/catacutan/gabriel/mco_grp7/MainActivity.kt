9b013d50ee8d2066edcb9b92f325d2ecb5894a62
GabCatacutan <gabriel_catacutan@dlsu.edu.ph>
2023-02-20 21:16:15 +0800

    Added hello :)

--- app/src/main/java/com/mobdeve/s13/catacutan/gabriel/mco_grp7/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/catacutan/gabriel/mco_grp7/MainActivity.kt
@@ -2,10 +2,12 @@ package com.mobdeve.s13.catacutan.gabriel.mco_grp7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("HELLO","HI")
     }
 }
